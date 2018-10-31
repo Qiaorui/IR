0812c582687d75342bf847d4ded2439e7db96693
qiaorui <qiaorui.xiang@gmail.com>
2018-10-31 14:22:03 +0100

    add arg parser

--- session3/PageRank.py
+++ session3/PageRank.py
@@ -1,64 +1,69 @@
 #!/usr/bin/python
 
+from pathlib import Path
 from collections import namedtuple
 import time
 import sys
 import numpy as np
+import argparse
+
 
 class Edge:
-    def __init__ (self, origin=None):
-        self.origin = None # write appropriate value
-        self.weight = 1 # write appropriate value
+    def __init__(self, origin=None):
+        self.origin = None  # write appropriate value
+        self.weight = 1  # write appropriate value
 
     def __repr__(self):
         return "edge: {0} {1}".format(self.origin, self.weight)
 
-    ## write rest of code that you need for this class
 
 class Airport:
-    def __init__ (self, iden=None, name=None):
+    def __init__(self, iden=None, name=None):
         self.code = iden
         self.name = name
         self.routes = []
         self.routeHash = dict()
         self.outweight = 0  # write appropriate value
         self.pageIndex = 0
+
     def __repr__(self):
         return "{0}\t{2}\t{1}".format(self.code, self.name, self.pageIndex)
+
 
 edgeList = [] # list of Edge
 edgeHash = dict() # hash of edge to ease the match
 airportList = [] # list of Airport
 airportHash = dict() # hash key IATA code -> Airport
 
-def readAirports(fd):
-    print ("Reading Airport file from {0}".format(fd))
-    airportsTxt = open(fd, "r")
+
+def read_airports(fd):
+    print("Reading Airport file from {0}".format(fd))
+    airports_file = open(fd, "r")
     cont = 0
-    for line in airportsTxt.readlines():
+    for line in airports_file.readlines():
         a = Airport()
         try:
             temp = line.split(',')
             if len(temp[4]) != 5:
                 raise Exception('not an IATA code')
-            a.name=temp[1][1:-1] + ", " + temp[3][1:-1]
-            a.code=temp[4][1:-1]
+            a.name = temp[1][1:-1] + ", " + temp[3][1:-1]
+            a.code = temp[4][1:-1]
         except Exception as inst:
             pass
         else:
             cont += 1
             airportList.append(a)
             airportHash[a.code] = a
-    airportsTxt.close()
-    print ("There were {0} Airports with IATA code".format(cont))
+    airports_file.close()
+    print("There were {0} Airports with IATA code".format(cont))
 
 
-def readRoutes(fd):
+def read_routes(fd):
     print ("Reading Routes file from {0}".format(fd))
     # write your code
-    routesTxt = open(fd, "r")
+    routes_file = open(fd, "r")
     cont = 0
-    for line in routesTxt.readlines():
+    for line in routes_file.readlines():
         e = Edge()
         try:
             temp = line.split(',')
@@ -87,14 +92,15 @@ def readRoutes(fd):
                 """
         except Exception:
             pass
-    routesTxt.close()
+    routes_file.close()
     # normalize weights of edges
     for code, airport in airportHash.items():
         for originCode, routeWeight in airport.routeHash.items():
             airport.routeHash[originCode] = float(routeWeight) / airportHash[originCode].outweight             
     print ("There were {0} routes with IATA codes that exist".format(cont))
 
-def computePageRanks():
+
+def compute_page_ranks(L, maxiter, epsilon, verbose):
     # a dictionary which stores the index of each airport and viceversa
     airportIndices = dict()
     index = 0
@@ -104,10 +110,8 @@ def computePageRanks():
         index += 1
     n = len(airportHash)
     P = np.array([1.0/n] * n, np.float64)
-    L = 0.9
-    maxiter = 10
+
     iter = 0
-    epsilon = 1e-4
     difference = n
     #try:
     while (iter < maxiter and difference > epsilon):
@@ -138,24 +142,39 @@ def computePageRanks():
     return iter
     #except Exception as inst:
      #   print(inst)
-    
+
+
 def outputPageRanks():
     # order the dictionary decreasingly by the pageIndex
     #sorted_d = sorted(airportHash.items(), key=lambda (k, v): v.pageIndex, reverse=True)
     sortedAirports = [(value.code, value.pageIndex) for (key, value) in sorted(airportHash.items(), key=lambda tup: tup[1].pageIndex, reverse=True)]
     #sortedAirports = sorted([(value.pageIndex, key) for (key,value) in airportHash.items()], reverse=True)
     print(sortedAirports[1:10])
-    
-def main(argv=None):
-    readAirports("airports.txt")
-    readRoutes("routes.txt")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-path", default=".", help="Input file directory path")
+    parser.add_argument("-max", default="1000", type=int, help="Maximum iteration for Page rank", action="store")
+    parser.add_argument("-eps", default="0.0001", type=float, help="Epsilon as threshold for convergence", action="store")
+    parser.add_argument("-df", default="0.9", type=float, help="Dumping factor", action="store")
+    parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    parser.add_argument("-w", dest="output_file", action="store", help="write to file")
+    args = parser.parse_args()
+
+    data_folder = Path(args.path)
+    if not data_folder.exists():
+        raise FileExistsError("No data folder found under " + args.path)
+
+    read_airports((data_folder / "airports.txt").absolute())
+    read_routes((data_folder / "routes.txt").absolute())
     
     time1 = time.time()
-    iterations = computePageRanks()
+    iterations = compute_page_ranks(args.df, args.max, args.eps, args.verbose)
     time2 = time.time()
     outputPageRanks()
-    print ("#Iterations:", iterations)
-    print ("Time of computePageRanks():", time2-time1)
+    print("#Iterations:", iterations)
+    print("Time of computePageRanks():", time2-time1)
     
 
 if __name__ == "__main__":
