1ecccc22160705c927209f45546dd345ce38518f
qiaorui <qiaorui.xiang@gmail.com>
2018-12-03 10:34:41 +0100

    add jupyter notebook

--- session5/main.py
+++ session5/main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     print("**************** Real World Test ****************")
     print("{:<20}{}".format("Small diameter", math.log(n) > diameter))
     print("{:<20}{}".format("High clustering", c > p))
-    print("{:<20}{}".format("Power law ", fit.alpha > 2))
+    print("{:<20}{}".format("Power law ", fit.alpha < 3))
     print("******************* Page Rank *******************")
     pr = nx.pagerank(g)
     max_color = max(pr.values())
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     for key, value in reversed(sorted_pr[-10:]):
         print("{:<30}:{:10.6f}".format(key, value))
 
-    #plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 10))
     nx.draw_kamada_kawai(g, node_size=20, node_color=color_list, cmap=plt.cm.Reds_r)
     plt.show()
     print("************** Community Detection **************")
@@ -65,7 +65,6 @@ if __name__ == '__main__':
     for i, count in sorted(partition_count.items(), key=lambda tup: tup[1]):
         print("Group {:<3}\t{}".format(i+1, count))
     pos = nx.kamada_kawai_layout(g)
-
 
     fig, ax = plt.subplots()
     plt.bar(partition_count.keys(), partition_count.values(), width=0.80, color='b')
@@ -76,7 +75,7 @@ if __name__ == '__main__':
     ax.set_xticklabels([c+1 for c in partition_count.keys()])
     plt.show()
 
-    #plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 10))
     nx.draw_networkx_edges(g, pos, alpha=0.5)
     nx.draw_networkx_nodes(g, pos, node_color=list(partition.values()), with_labels=False, node_size=20, cmap=plt.cm.jet)
     plt.axis('off')
