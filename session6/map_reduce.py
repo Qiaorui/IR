17157808cd6fb9ca7b47572c242246e6bc7ed562
qiaorui <qiaorui.xiang@gmail.com>
2018-12-13 20:07:51 +0100

    store all computed association rules into mongo db

--- session6/map_reduce.py
+++ session6/map_reduce.py
@@ -41,20 +41,50 @@ if __name__ == '__main__':
     db = client.ir
 
     try:
-        """
-        if "market" in db.collections_names():
+
+        if "market" in db.list_collection_names():
             db.market.remove()
-        if "counts" in db.collections_names():
+        if "counts" in db.list_collection_names():
             db.counts.remove()
-        """
+        if "rules" in db.list_collection_names():
+            db.counts.remove()
+
         print("****************** Reading Data ******************")
         read("groceries.csv")
         print(db.market.count(), "documents have been inserted")
 
         print("******************* Map Reduce *******************")
         r = db.market.map_reduce(mapper, reducer, "counts")
-        print("finished reducing")
-        print(r.count(), "documents have been inserted")
+        print(r.count(), "counted items have been inserted")
+
+        print("********* Compute support and confidence *********")
+
+        # Read map reduce results and store into python structure
+        items = {}
+        pairs = {}
+        for doc in db.counts.find():
+            if isinstance(doc['_id'], str):
+                items[doc['_id']] = doc['value']
+            else:
+                pairs[(doc['_id']['word1'], doc['_id']['word2'])] = doc['value']
+
+        print(len(items), "unique items found")
+
+        # Compute association rule
+        sup = {}
+        conf = {}
+        N = db.market.count()
+        for (a, b), value in pairs.items():
+            sup[a, b] = sup[b, a] = value / N
+            conf[a, b] = value / items[a]
+            conf[b, a] = value / items[b]
+
+        # Write the computed association rules into mongo
+        for comb in sup.keys():
+            rule = {"rule": comb, "confidence": conf[comb], "support": sup[comb]}
+            db.rules.insert(rule)
+        print(db.rules.count(), "rules have been inserted")
+
     except Exception as e:
         print("Exception:", e)
         print("Self-destroying...")
